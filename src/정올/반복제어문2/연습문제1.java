1f2e659c4d0b6666571c72eb848b5bdd2ce99ef6
Dahye021 <hnu4051@naver.com>
2025-07-28 12:46:39 +0900

    day04

---
+++ src/정올/반복제어문2/연습문제1.java
@@ -0,0 +1,14 @@
+package 정올.반복제어문2;
+
+import java.util.Scanner;
+
+public class 연습문제1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        
+        for (int i = 1 ; i <= number; i++){
+            System.out.println("C언어 프로그래밍");
+        }
+    }
+}
---
+++ src/정올/반복제어문2/연습문제3_1.java
@@ -0,0 +1,16 @@
+package 정올.반복제어문2;
+
+import java.util.stream.IntStream;
+
+public class 연습문제3_1 {
+    public static void main(String[] args) {
+        for(int i = 1 ; i <= 20 ; i++){
+            if (i%2 ==1) System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("===================");
+
+        IntStream intStream = IntStream.rangeClosed(1,20);
+        intStream.filter(n-> n%2 == 1).forEach(e-> System.out.println(e+ " "));
+    }
+}
