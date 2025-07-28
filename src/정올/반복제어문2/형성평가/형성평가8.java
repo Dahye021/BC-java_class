5402139105e585deafca778631f185a2bfc0e46b
Dahye021 <hnu4051@naver.com>
2025-07-28 15:56:20 +0900

    day04

---
+++ src/정올/반복제어문2/형성평가/형성평가8.java
@@ -0,0 +1,17 @@
+package 정올.반복제어문2.형성평가;
+
+public class 형성평가8 {
+    public static void main(String[] args) {
+        int row = 3;
+        int col = 4;
+
+        for(int i = 1; i <= row; i++){
+
+            for(int j = 1; j <= col; j++){
+                System.out.print(i*j+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
