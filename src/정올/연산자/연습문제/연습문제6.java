be4e9495f82ae91cf2ebf5924376a4aa299ec050
Dahye021 <hnu4051@naver.com>
2025-07-28 22:54:59 +0900

    day04

--- src/정올/연산자/연습문제/연습문제6.java
+++ src/정올/연산자/연습문제/연습문제6.java
@@ -1,7 +1,18 @@
 package 정올.연산자.연습문제;
 
+import java.util.Scanner;
+
 public class 연습문제6 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
+        int result1 = (a > b) ? 1 : 0;
+        int result2 = (b >= c) ? 1 : 0;
+        int result3 = (a <= b) ? 1 : 0;
+        int result4 = (b < c) ? 1 : 0;
+        System.out.printf("%d %d %d %d", result1, result2, result3, result4);
     }
 }
---
+++ src/정올/연산자/형성평가/형성평가1.java
@@ -0,0 +1,7 @@
+package 정올.연산자.형성평가;
+
+public class 형성평가1 {
+    public static void main(String[] args) {
+
+    }
+}
