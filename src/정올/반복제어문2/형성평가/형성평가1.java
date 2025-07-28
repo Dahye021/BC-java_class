16a751c88f6e8f5cec8c7b199b1b179d83fd29cb
Dahye021 <hnu4051@naver.com>
2025-07-28 14:46:38 +0900

    day04

---
+++ src/정올/반복제어문2/형성평가/형성평가1.java
@@ -0,0 +1,15 @@
+package 정올.반복제어문2.형성평가;
+
+import java.util.Scanner;
+
+public class 형성평가1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+
+        for (int i = 1; i <= num1 ; i++){
+            System.out.println("JUNGOL");
+        }
+    }
+}
---
+++ src/정올/반복제어문2/형성평가/형성평가4.java
@@ -0,0 +1,19 @@
+package 정올.반복제어문2.형성평가;
+
+import java.util.Scanner;
+
+public class 형성평가4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < n; i ++){
+            sum += sc.nextInt();
+        }
+        double average = (double) sum / n;
+
+        System.out.printf("%.2f", average);
+    }
+}
