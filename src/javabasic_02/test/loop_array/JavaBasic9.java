12b59dfa8bbfdf918fdcbab0461b8bbb07706e40
Dahye021 <hnu4051@naver.com>
2025-07-31 15:37:32 +0900

    day07-05

--- src/javabasic_02/test/loop_array/JavaBasic9.java
+++ src/javabasic_02/test/loop_array/JavaBasic9.java
@@ -1,7 +1,17 @@
 package javabasic_02.test.loop_array;
 
+import java.util.Scanner;
+
 public class JavaBasic9 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        int[] scores = null;
+        int studentCount = 0;
+        boolean run = true;
+
+        while (run) {
+
+        }
     }
 }
