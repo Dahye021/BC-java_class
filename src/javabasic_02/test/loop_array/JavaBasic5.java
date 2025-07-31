91033447fb1313b183a3ccf95380848aa6f5c823
Dahye021 <hnu4051@naver.com>
2025-07-31 17:34:07 +0900

    day07-06

--- src/javabasic_02/test/loop_array/JavaBasic5.java
+++ src/javabasic_02/test/loop_array/JavaBasic5.java
@@ -2,7 +2,7 @@ package javabasic_02.test.loop_array;
 
 public class JavaBasic5 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++){
+        for (int i = 1; i <= 5; i++){ //줄 수
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
