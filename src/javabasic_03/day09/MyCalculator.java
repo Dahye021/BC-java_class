82a31296e8c3399399f7aab0c0b9b54b5ec67e39
Dahye021 <hnu4051@naver.com>
2025-08-04 12:42:17 +0900

    day09-04

---
+++ src/javabasic_03/day09/MyCalculator.java
@@ -0,0 +1,11 @@
+package javabasic_03.day09;
+
+public class MyCalculator {
+    int[] numbers; //수를 저장하는 정수형 배열
+
+    public void adder(int[] datas){
+        int n1 = datas[0];
+        int n2 = datas[1];
+        System.out.println(datas[0] + datas[1]);
+    }
+}
