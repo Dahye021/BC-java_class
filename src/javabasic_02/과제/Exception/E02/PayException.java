717f5888e7dadbfddc74381e77307f2dc5b78a14
Dahye021 <hnu4051@naver.com>
2025-08-12 16:47:01 +0900

    day14-03

---
+++ src/javabasic_02/과제/Exception/E02/PayException.java
@@ -0,0 +1,9 @@
+package javabasic_02.과제.Exception.E02;
+
+// 사용자 정의 Exception : 수정하지 말고 그대로 사용하세요.
+public class PayException extends Exception {
+
+    public PayException(String message){
+        super(message);
+    }
+}
---
+++ src/javabasic_02/과제/Exception/E02/Payment.java
@@ -0,0 +1,13 @@
+package javabasic_02.과제.Exception.E02;
+
+public abstract class Payment implements Payable {
+    protected String shopName;
+    protected String productName;
+    protected long productPrice;
+
+    public Payment(String shopName, String productName, long productPrice) {
+        this.shopName = shopName;
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+}
