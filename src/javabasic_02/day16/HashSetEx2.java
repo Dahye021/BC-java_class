c765887945101b55f7a29a1ac9febb64833ada92
Dahye021 <hnu4051@naver.com>
2025-08-14 10:52:35 +0900

    day16-02

--- src/javabasic_02/day16/HashSetEx2.java
+++ src/javabasic_02/day16/HashSetEx2.java
@@ -1,6 +1,7 @@
 package javabasic_02.day16;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class HashSetEx2 {
@@ -13,5 +14,18 @@ public class HashSetEx2 {
 
         int size = set.size();
         System.out.println(size);
+
+        //Iterator 적용하여 객체 처리
+        Iterator<Member> iterator = set.iterator();
+
+        while (iterator.hasNext()){
+            Member member = iterator.next();
+            System.out.println(member.name + " " + member.age);
+        }
+        System.out.println();
+
+        for (Member member : set) {
+            System.out.println(member.name + " " + member.age);
+        }
     }
 }
