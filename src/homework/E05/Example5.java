6aa053c52b12fa423eaebc3dba45e7718427c1eb
Dahye <hnu4051@naver.com>
2025-08-21 22:11:05 +0900

    homework

---
+++ src/homework/E05/Example5.java
@@ -0,0 +1,17 @@
+package homework.E05;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Example5 {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList(
+                "This is a java book",
+                "Lambda Expressions",
+                "Java8 supports lambda expressions"
+        );
+        list.stream()
+                .filter(s -> s.startsWith("Java"))
+                .forEach(System.out::println);
+    }
+}
