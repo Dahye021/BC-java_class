8fc1a9b65a8720ed38513a3a84354bb6afeab5ff
DaHye <heny993472@gmail.com>
2025-08-25 10:51:49 +0900

    day21-03

--- src/Java_basic_04/day21/outputstream/WriterEx02.java
+++ src/Java_basic_04/day21/outputstream/WriterEx02.java
@@ -11,7 +11,7 @@ public class WriterEx02 {
             //출력 스트림 생성 C:Temp/test1.txt에 1byte 데이터를 저장
             OutputStream os = new FileOutputStream("C:/Temp/test2.db"); // 데이터 도착지를 test2.db 바이트 출력 스트림 생성
             //내보낼 데이터를 지정
-            byte[] array = {10,20,30};
+            byte[] array = {10,20,30,40,50,60};
             //데이터를 스트림에 보내서 해당 목적지에 쓴다 / 배열의 모든 바이트를 출력
             os.write(array);
             //스트림의 버퍼를 비운다 (내부 버퍼에 잔류하는 바이트를 출력하고 버퍼를 비움)
